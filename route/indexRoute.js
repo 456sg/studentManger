c629209852682400eba2a9e5d37e277636dd3a35
Your Name <you@example.com>
2018-07-17 20:22:45 +0800

    完成后台管理系统

--- route/indexRoute.js
+++ route/indexRoute.js
@@ -18,7 +18,7 @@ router.get('/insert', function(req, res) {
     //接收数据
     console.log(req.query);
     //插入数据
-    myT.insert('userInfo',req.query,(err,result)=>{
+    myT.insert('userList',req.query,(err,result)=>{
         if(!err)res.json({
             mess:'新增成功',
             code:200
@@ -31,7 +31,7 @@ router.get('/delte', function(req, res) {
     console.log(req.query.id);
     let id=objectID(req.query.id);
     //删除数据
-    myT.delete('userInfo',{_id:id},(err,result)=>{
+    myT.delete('userList',{_id:id},(err,result)=>{
         if(!err)res.json({
             mess:'删除成功',
             code:200
@@ -49,7 +49,7 @@ router.get('/update', function(req, res) {
     let sex = req.query.sex;
     let id=objectID(req.query.id);
     //更新数据
-    myT.update('userInfo',{_id:id},{address,age,introduction,name,phone,sex},(err,result)=>{
+    myT.update('userList',{_id:id},{address,age,introduction,name,phone,sex},(err,result)=>{
         if(!err)res.json({
             mess:'修改成功',
             code:200
@@ -60,7 +60,7 @@ router.get('/update', function(req, res) {
 router.get('/find', function(req, res) {
     //接收数据
     //更新数据
-    myT.find('userInfo',{},(err,docs)=>{
+    myT.find('userList',{},(err,docs)=>{
         if(!err)res.json({
             mess:'查询成功',
             code:200,
@@ -68,6 +68,30 @@ router.get('/find', function(req, res) {
         })
     })
 });
+
+router.get('/search',(req,res)=>{
+    // 定义查询的对象
+    let query  ={};
+    console.log(req.query.userName)
+    // 用户名过来
+    if(req.query.userName){
+        query.name = new RegExp(req.query.userName);
+    }
+    // 有id过来
+    if(req.query.id){
+        query._id = objectID(req.query.id);
+    }
+    // console.log(name);
+    // 来就给你所有的东西
+    // mongoDB模糊查询 使用正则表达式
+    myT.find('userList',query,(err,docs)=>{
+        if(!err)  res.json({
+            mess:"数据",
+            code:200,
+            list:docs
+        });
+    })
+})
 
 
 
